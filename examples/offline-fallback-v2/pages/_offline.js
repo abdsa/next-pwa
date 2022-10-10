b5a8440aa0a5cb133f9ab8db08705c158c090cd4
abdsa <abd.sab111@gmail.com>
2022-10-10 05:36:26 +0300

    hello there

--- examples/offline-fallback-v2/pages/_offline.js
+++ examples/offline-fallback-v2/pages/_offline.js
@@ -5,7 +5,7 @@ const Fallback = () => (
     <Head>
       <title>next-pwa example</title>
     </Head>
-    <h1>This is offline fallback page</h1>
+    <h1>This is offline fallback page, boo</h1>
     <h2>When offline, any page route will fallback to this page</h2>
   </>
 )
